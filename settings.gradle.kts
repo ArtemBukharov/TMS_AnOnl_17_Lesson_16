9c304add0ed874b1c70b14ee6853677553a6eda1
Artem Bukharov <artem.bukharov.2018@gmail.com>
2024-02-28 18:29:33 +0300

    nenujnoye

---
+++ settings.gradle.kts
@@ -0,0 +1,19 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "TMS_AnOnl_17_Lesson_16"
+include(":app")
+include(":app")
+ 
